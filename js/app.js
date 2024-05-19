f967fbe99423a1263bfdff328498c0a992054b9b
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-05-19 19:18:43 +0000

    selecting elements ch2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 // CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
