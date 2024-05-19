becb6db8d1ec18e9ede27b0ba893cada3ab73506
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-05-19 19:34:18 +0000

    add to do Ch4

--- js/app.js
+++ js/app.js
@@ -7,3 +7,14 @@ const input = document.getElementById("input");
 //ch3
 const element = document.getElementById("element");
 element.insertAdjacentHTML(position, text);
+//ch4
+function addToDo(toDo){
+const text = `<li class="item">
+    <i class="co fa fa-circle-thin" job="complete"></i>
+    <p class="text"> ${toDo} </p>
+    <i class="de fa fa-trash-o" job="delete"></i>
+</li>`
+const position="beforeend";
+list.insertAdjacentHTML(position,text);
+}
+addToDo("Drink Coffee");
