e8279c74cbdb9b592f15d53e2e82a1c2913032a9
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-05-19 19:24:24 +0000

    innertadjacentHtml() Ch3

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 // CODE EXPLAINED channel
+//ch2
 const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
+//ch3
+const element = document.getElementById("element");
+element.insertAdjacentHTML(position, text);
